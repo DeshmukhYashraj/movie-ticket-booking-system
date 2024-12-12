cb5db616722902bab0bbb8807c061744fe4b6d08
Sanket Prabhu Pawar <Sanket Prabhu Pawar@LAPTOP-TOVV455I>
2024-12-12 15:23:42 +0530

    after language

--- Movie_Ticket_Booking/src/main/java/com/movie/client/MovieTicketBookingApp.java
+++ Movie_Ticket_Booking/src/main/java/com/movie/client/MovieTicketBookingApp.java
@@ -2,25 +2,16 @@ package com.movie.client;
 
 import org.apache.log4j.Logger;
 
-import com.movie.config.DBConfig;
 import com.movie.config.DBState;
 import com.movie.config.LoggerApp;
+import com.movie.operator.AdminModel;
 
 public class MovieTicketBookingApp extends DBState {
 
-	Logger logger = LoggerApp.getLogger();
+	static AdminModel admin = new AdminModel();
+	static Logger logger = LoggerApp.getLogger();
 
 	public static void main(String[] args) {
-		try {
-			config = DBConfig.getInstance();
-			con = DBConfig.getCon();
-			if (con != null)
-				System.out.println("connected");
-			else
-				System.out.println("not connected");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		admin.adminOperator();
 	}
 }
--- Movie_Ticket_Booking/src/main/java/com/movie/operator/AdminModel.java
+++ Movie_Ticket_Booking/src/main/java/com/movie/operator/AdminModel.java
@@ -1,6 +1,34 @@
 package com.movie.operator;
 
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import com.movie.config.LoggerApp;
+import com.movie.model.Language;
+
 public class AdminModel {
-	
+
+	static Logger logger = LoggerApp.getLogger();
+
+	public void adminOperator() {
+		Scanner scn = new Scanner(System.in);
+		do {
+			System.out.println("<<Welcome Admin>>");
+			System.out.println("1 :: Add Movie Language :: ");
+			int value = scn.nextInt();
+			switch (value) {
+			case 1: {
+				System.out.println("Enter Language Name :: ");
+				String langName = scn.nextLine();
+				Language lang = new Language(langName);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + value);
+			}
+		} while (true);
+
+	}
 
 }
